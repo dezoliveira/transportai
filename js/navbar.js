7aeccde362fbcf40be435280564a828410a026a5
Andres Oliveira <andresoliveira@protonmail.com>
2023-11-22 11:15:00 -0300

    Date validation e swal

--- js/navbar.js
+++ js/navbar.js
@@ -13,22 +13,38 @@ btnSearch.addEventListener('click', () => {
   let startDateVal = startDate.value
   let endDateVal = endDate.value
 
-  datePeriod.textContent = `Período de ${startDateVal} á ${endDateVal}`
+  console.log(startDate.value)
+  console.log(endDate.value)
+
+  if(!startDateVal.length) {
+    // return alert('Digite a data Inicial')
+    Swal.fire({
+        title: 'Erro!',
+        text: 'Digite a Data Inicial',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+    })
+    return
+  }
+  
+  if(!endDateVal.length) {
+    // return alert('Digite a data Final')
+    Swal.fire({
+        title: 'Error!',
+        text: 'Digite a Data Final',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+    })
+    return
+  }
+
+  let arr1 = startDateVal.split('-')
+  let arr2 = endDateVal.split('-')
+  
+  let newStartDate = arr1[0] + '/' + arr1[1] + '/' + arr1[2]
+  let newEndDate = arr2[0] + '/' + arr2[1] + '/' + arr2[2]
+  console.log(arr1)
+  console.log(arr2)
+
+  datePeriod.textContent = `Período de ${newStartDate} á ${newEndDate}`
 })
-
-const swapTabs = (tab) => {
-    let selectedTab = tab
-    let tabId = tab.id.replace('tab-', '')
-    const activeTabs = document.querySelectorAll('.active') 
-    
-    for(i=0; i < activeTabs.length; i++){
-        if(activeTabs[i].className.includes() === 'active') {
-            activeTabs.classList.remove('active')
-        }
-
-                    console.log(activeTabs[i].className)
-    }
-    
-    document.querySelector('#' + tabId).classList.add('active')
-    selectedTab.classList.add('active')
-}
